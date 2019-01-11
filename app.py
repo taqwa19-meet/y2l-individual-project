c4c138d8a4b5ca968562fda0a6c6a794bf37042b
aboud20-meet <aboud20@meet.mit.edu>
2019-01-11 20:13:34 +0200

    navbar added

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World test!'
+def home_page():
+    return render_template("project-home.html")
+
+
+@app.route('/About us')
+def info():
+	return render_template("project_info")
+
+	pass
 
 if __name__ == '__main__':
     app.run(debug=True)
